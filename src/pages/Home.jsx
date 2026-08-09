ed6cdb474b55be3944a25880c9af08f1ce0ecc87
tshepiem.dev <tshepiem.dev@gmail.com>
2026-08-09 21:26:35 +0200

    Index og update v2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
     <main className={styles.home}>
       <PageHelmet
         title="tshepiem.dev"
-        description="Creative developer to building scalable, high-performance digital solutions with clean design and efficient engineering."
+        description="Creative developer building scalable, high-performance digital solutions with clean design and efficient engineering."
         image={ogImages.home}
         url={window.location.href}
         keywords="hire developer, software developer, web developer, React developer, Node.js developer, JavaScript, TypeScript, C#, South Africa"
